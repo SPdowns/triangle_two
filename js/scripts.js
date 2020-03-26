c596573c79c40bd7b2c7ccd12fdb4e99926f7554
Sean Downs <spdowns@protonmail.com>
2020-03-26 15:46:45 -0700

    adds not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,18 @@ $(document).ready(function() {
 
     var answer;
 
-    if (right === center && right === left) {
+
+    if ( right + left   <= center
+     ||  right + center <= left
+     ||   left + center <= right){
+      answer="not a triangle"
+    } else if (right === center && right === left) {
       answer = "an Equilateral";
     } else if (right === center || right === left || center === left) {
       answer = "an isoscoles"
     } else if (right !== left || left !== center || right !== center) {
       answer = "a scalene"
-    } else if (right + left <= center || right + center <= left || left + center <= right){
-      answer="not a triangle"
-    }
+    } 
   
     $("#typeOfTriangle").text(answer);
   });
