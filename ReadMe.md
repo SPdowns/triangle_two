3b2f1ec955943c8e06404a270590f2520bc74f99
Sean Downs <spdowns@protonmail.com>
2020-04-14 09:48:04 -0700

    updated bootstrap to 4.4.1 and jquery is now defined when our build

---
+++ ReadMe.md
@@ -0,0 +1,37 @@
+# _Triangle_
+
+####Intermediate JavaScript,  April 13th, 2020
+
+#### By _Tyler Bates & Sean Downs
+
+## Description
+
+An interactive site returning the type of triangle based on user input side lengths
+## Logic Specifications
+```
+Logic:           | Example:   | Output
+-------------    | -----------| ----- 
+
+
+```
+
+## Setup/Installation Requirements
+
+Enter the url "[]" into your browser.
+
+For additional information on this project, you can:
+* _clone repository from_ []
+* _find in directory_
+* _open index in local server_
+* _enjoy_
+
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScript, JQuery, Jest, node, webpack.
+
+### License
+
+
+Copyright (c) 2020 **_Tyler Bates & Sean Downs @ Epicodus_**
+```
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+    var right = parseInt($("input#side1").val());
+    var center = parseInt($("input#side2").val());
+    var left = parseInt($("input#side3").val());
+
+    var answer;
+
+
+    if ( right + left   <= center
+     ||  right + center <= left
+     ||   left + center <= right){
+      answer="not a triangle"
+    } else if (right === center && right === left) {
+      answer = "an Equilateral";
+    } else if (right === center || right === left || center === left) {
+      answer = "an isoscoles"
+    } else if (right !== left || left !== center || right !== center) {
+      answer = "a scalene"
+    } 
+  
+    $("#typeOfTriangle").text(answer);
+  });
+});
+
+
+
+//("This is a " +  + " triangle")
